4b8c3444f65599fd74d6474af754ceb04ac6a5c8
Vegard Bønes <vegard.bones@met.no>
2010-03-18 14:34:06 +0000

    creating directory before attempting to place a file in it
    git-svn-id: https://svn.met.no/kvoss/kvAgregated/trunk@1296 422d51e2-5941-41ac-8e65-d5b5931239be

--- kvAgregateDbInit.sh
+++ kvAgregateDbInit.sh
@@ -26,5 +26,5 @@
 #  with KVALOBS; if not, write to the Free Software Foundation Inc., 
 #  51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
-
+mkdir -p `dirname $1`
 sqlite3 $1 "CREATE TABLE data (stationid INTEGER NOT NULL,obstime TIMESTAMP NOT NULL,original FLOAT NOT NULL,paramid INTEGER NOT NULL,tbtime TIMESTAMP NOT NULL,typeid INTEGER NOT NULL,sensor CHAR(1) DEFAULT '0',level INTEGER DEFAULT 0,corrected FLOAT NOT NULL,controlinfo CHAR(16) DEFAULT '0000000000000000',useinfo CHAR(16) DEFAULT '0000000000000000',cfailed TEXT DEFAULT NULL,UNIQUE ( stationid, obstime, paramid, level, sensor, typeid ));"
