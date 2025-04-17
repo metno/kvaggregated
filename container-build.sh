57f73c81c42a7d0b9fb6d7924fca3ef9dcb905a7
Børge Moe <borge.moe@met.no>
2025-04-17 10:30:34 +0200

    Refactor the tag logic

--- container-build.sh
+++ container-build.sh
@@ -6,7 +6,6 @@ kvuserid=5010
 mode="test"
 target=kvaggregated
 tag=latest
-tag_and_latest=false
 kvcpp_tag=latest
 default_os=noble
 os=noble
@@ -18,6 +17,7 @@ nocache=
 BUILDDATE=$(date +'%Y%m%d')
 VERSION="$(./version.sh)"
 KV_BUILD_DATE=${KV_BUILD_DATE:-}
+tags=""
 
 if [ -n "${KV_BUILD_DATE}" ]; then
   BUILDDATE=$KV_BUILD_DATE
@@ -27,7 +27,7 @@ fi
 use() {
 
   usage="\
-Usage: $0 [--help] [--no-cache] [--staging|--prod|--test] [--kvcpp-tag] [--tag tag] [--tag-with-build-date] 
+Usage: $0 [--help] [options] 
 
 This script build a kvagregated container. 
 
@@ -40,21 +40,25 @@ to the registry.
 Options:
   --help        display this help and exit.
   --tag tagname tag the image with the name tagname, default latest.
-  --tag-with-build-date 
-                tag with version and build date on the form version-YYYYMMDD 
-                and set latest. If the enviroment variable KV_BUILD_DATE is set use
-                this as the build date. Format KV_BUILD_DATE YYYYMMDD.
-  --tag-version Use version from configure.ac as tag. Also tag latest.
+  --tag-and-latest tagname 
+                tag the image with tagname and also create latest tag.
+  --tag-with-build-date
+                Creates three tags: ${VERSION}, latest and a ${VERSION}-${BUILDDATE}.
+                If the enviroment variable KV_BUILD_DATE is set use
+                this as the build date. Format KV_BUILD_DATE YYYYMMDD. 
   --staging     build and push to staging.
   --prod        build and push to prod.
   --test        only build, default
   --kvcpp-tag tag  Use this kvcpp-runtime/kvcpp-dev as base image. Default: $kvcpp_tag
   --no-cache    Do not use the docker build cache.
-  --only-build  Stop after building.
-  --only-push   Only push a previous build to registry. Must use the same flags as when builing.
+  --build-only  Stop after building.
+  --push-only   Only push a previous build to registry. Must use the same flags as when builing.
   --print-version-tag
                 Print the version and build date on the form version-YYYYMMDD.
 
+  The following opptions is mutally exclusive: --tag, --tag-and-latest, --tag-with-build-date
+  The following options is mutally exclusive: --build-only, --push-only
+  The following options is mutally exclusive: --staging, --prod, --test
 "
 echo -e "$usage\n\n"
 
@@ -64,6 +68,7 @@ while test $# -ne 0; do
   case $1 in
     --tag) 
         tag=$2
+        tag_counter=$((tag_counter + 1))
         shift
         ;;
     --help) 
@@ -72,30 +77,38 @@ while test $# -ne 0; do
     --staging) mode="staging";;
     --prod) mode="prod";;
     --test) mode="test";;
-    --tag-version) 
-        tag="$VERSION"
-        tag_and_latest=true
+    --tag-and-latest) 
+        tag="$2"
+        tags="latest"
+        tag_counter=$((tag_counter + 1))
+        shift
         ;;
     --tag-with-build-date)
-        tag="$VERSION-$BUILDDATE"
-        tag_and_latest=true;;
+        tags="latest $VERSION-$BUILDDATE"
+        tag_counter=$((tag_counter + 1))
+        ;;
     --kvcpp-tag) 
         kvcpp_tag="$2"
         shift 
         ;;
     --no-cache) nocache="--no-cache";;
-    --only-build) push="false";;
-    --only-push) build="false";;
+    --build-only) push="false";;
+    --push-only) build="false";;
     --print-version-tag)
         echo "$VERSION-$BUILDDATE"
         exit 0;;
     -*) use
-      echo "Invalid option $1"
-      exit 1;;  
-    *) targets="$targets $1";;
+        echo "Invalid option $1"
+        exit 1;;  
+      *) targets="$targets $1";;
   esac
   shift
 done
+
+if [ $tag_counter -gt 1 ]; then
+  echo "Only one of --tag, --tag-and-latest or --tag-with-build-date can be used"
+  exit 1
+fi
 
 echo "VERSION: $VERSION"
 echo "mode: $mode"
@@ -123,16 +136,17 @@ if [ "$build" = "true" ]; then
   docker build $nocache --build-arg BASE_IMAGE_TAG="${kvcpp_tag}" --build-arg REGISTRY=${registry} \
     --build-arg "kvuser=$kvuser" --build-arg "kvuserid=$kvuserid" \
     -f Dockerfile --tag "${registry}${target}:${tag}" .
-
-  if [ ${tag} != "latest" ]; then
-    docker tag "${registry}${target}:$tag" "${registry}${target}:latest"
-  fi
+  for tagname in $tags; do
+    echo "Tagging: ${registry}${target}:$tagname"
+    docker tag "${registry}${target}:$tag" "${registry}${target}:$tagname"
+  done
 fi
 
 if [ "$mode" != "test" ] && [ "$push" = "true" ]; then 
+  echo "Pushing: ${registry}${target}:$tag"
   docker push "${registry}${target}:${tag}"
-  if [ "$tag_and_latest" = "true" ] &&  [ "${tag}" != "latest" ]; then
-    docker push "${registry}${target}:latest"
-  fi
+  for tagname in $tags; do
+      echo "Pushing: ${registry}${target}:$tagname"
+      docker push "${registry}${target}:$tagname"
+  done
 fi
-
